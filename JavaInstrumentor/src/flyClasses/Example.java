e65b5b9d3ac3c770e4e113578cfd4c132fa72eac
alon danielli <guy7145@gmail.com>
2018-07-30 13:13:55 +0300

    added example file

---
+++ JavaInstrumentor/src/flyClasses/Example.java
@@ -0,0 +1,72 @@
+package flyClasses;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import polyglot.ast.SourceFile;
+
+public class Example {
+	public static final String CLASS_NAME = "flyClasses.Example";
+	private String functionName;
+	private List<String> stateReports;
+	
+	private Map<String, Object> locals;
+	
+	public Example(String functionName) {
+		this.functionName = functionName;
+		this.stateReports = new LinkedList<>();
+		locals = new HashMap<>();
+	}
+	
+	public void addLine(String line) {
+		this.stateReports.add(line);
+	}
+	
+	public void InitLocal(String name, boolean isPrimitive) {
+		locals.put(name, isPrimitive ? 0 : null);
+	}
+	
+	private static String getEqualityString(String var, Object val) {
+		return String.format("%s == %s", var, val);
+	}
+	
+	public void ReportState() {
+		List<String> stateLines = new LinkedList<>();
+		for (String localName : locals.keySet()) stateLines.add(getEqualityString(localName, locals.get(localName)));
+		addLine(String.format("[%s]", String.join(" && ", stateLines)));
+	}
+
+	public void UpdateAssignmentPrimitive(String varName, int val) {
+		locals.put(varName, val);
+		ReportState();
+	}
+
+	public void UpdateAssignmentObject(String varName, Object val) {
+		locals.put(varName, val);
+		ReportState();
+	}
+
+	public void UpdateInvoke(String methodName) {
+//		addLine(String.format("%s", methodName));
+		
+		// nothing to do here either, apparently
+	}
+
+	public void UpdateReturn() {
+		// nothing to do here, apparently
+	}
+	
+	public String getFunctionName() {
+		return this.functionName;
+	}
+	
+	public String getExampleText() {
+		return String.format("example {\n\t%s\n}", String.join(" ->\n\t", stateReports));
+	}
+}
